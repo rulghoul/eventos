191c883acc179942beac0ee8e325f469e5b066a3
Raúl Pérez Durán <ghoulrul@gmail.com>
2022-11-14 13:07:04 -0600

    Se regresa a la configuracion por default el archivo wsgi.py

--- eventos/eventos/wsgi.py
+++ eventos/eventos/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventos.settings')
 
-import django.core.handlers.wsgi
-
-application = django.core.handlers.wsgi.WSGIHandler()
+application = get_wsgi_application()
